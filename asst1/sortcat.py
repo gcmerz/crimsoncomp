62892cd8b6808fe254d0e93865dacba097150a6c
Zennie Wey <zennie_wey@college.harvard.edu>
2017-02-19 11:07:14 -0500

    no cmd line argument

--- asst1/sortcat.py
+++ asst1/sortcat.py
@@ -1,1 +1,11 @@
-from collections import Counter
+def sortcat(num, *argv):
+    sList = []
+    for arg in argv:
+        sList.append(arg)
+    sList.sort(key=len, reverse=True)
+    s = ""
+    for i in range(len(sList)):
+        if i < num:
+            s += sList[i]
+    print s
+sortcat(1, 'abc', 'c')
