203295a23f97d4434faeae803a5c01b06573a78f
retagaine <eniagater@gmail.com>
2017-02-20 16:03:59 -0500

    fixing stuff

--- asst2/models.py
+++ asst2/models.py
@@ -1,6 +1,6 @@
 
 from datetime import date
-
+from PIL import Image
 
 class Content(object):
     # list to keep track of all pieces of content
@@ -12,7 +12,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = datetime.date(year, month, day)
 
         # list of contirbutors
         self.contributors = contributors
@@ -24,5 +24,41 @@ class Content(object):
 
 # TODO: Define an Article class that extends the Content class
 
+class Article(Content):
+    def __init__(self, headline, content):
+        self.headline = headline;
+        self.content = content;
+
+    def show(self):
+        print headline
+        print "-" * len(headline)
+        print content
 
 # TODO: Define a Picture class that extends the Content class
+
+class Picture(Content):
+    def __init__(self, title, caption, path):
+        self.title = title;
+        self.caption = caption;
+        self.path = path;
+
+    def show(self):
+        print title
+        print "-" * len(title)
+        im = Image.open(path)
+        im.show()
+        print caption
+
+# Some code that parses JSON is: JSON.parse (thank u javascript)
+
+
+
+
+
+
+
+
+
+
+
+
