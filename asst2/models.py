8db9888a730c2b1b8e6bc27e8c17a0d1ea4f66f5
Mustafa Bal <mbal@college.harvard.edu>
2017-02-23 15:34:40 -0500

    done

--- asst2/models.py
+++ asst2/models.py
@@ -1,6 +1,6 @@
 
 from datetime import date
-
+from PIL import Image
 
 class Content(object):
     # list to keep track of all pieces of content
@@ -12,7 +12,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = [year, month, day]
 
         # list of contirbutors
         self.contributors = contributors
@@ -24,5 +24,21 @@ class Content(object):
 
 # TODO: Define an Article class that extends the Content class
 
+class Article(Content):
+    def __init__(self, headline, content):
+        self.content = content
+        self.headline = headline
 
+    def show(self):
+        print("Headline: {}\n".format(headline))
+        print("Content: {}\n".format(content))
 # TODO: Define a Picture class that extends the Content class
+class Picture(Content):
+    def __init__(self, title, caption, path, show):
+        self.title = title
+        self.caption = caption
+        self.path = path
+
+    def show(self):
+        image = Image.open(path)
+        image.show()
