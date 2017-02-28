f5b61b9641e396a853736739afde1951d340c7ee
Aaron Shi <Aaron@Aarons-MacBook-Pro-2.local>
2017-02-27 21:07:48 -0500

    finished

--- asst2/models.py
+++ asst2/models.py
@@ -12,7 +12,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = date(year, month, day)
 
         # list of contirbutors
         self.contributors = contributors
@@ -23,6 +23,24 @@ class Content(object):
 
 
 # TODO: Define an Article class that extends the Content class
+class Article(Content):
+    def __init__(self, year, month, day, contributors, headline, content):
+        self.headline = headline
+        self.content = content
 
+        super(Article, self).__init__(year, month, day, contributors)
+
+    def show(self):
+        raise NotImplementedError
 
 # TODO: Define a Picture class that extends the Content class
+
+class Picture(Content):
+    def __init__(self, year, month, day, title, caption, path, contributors):
+        self.title = title
+        self.caption = caption
+        self.path = path
+        super(Picture, self).__init__(year, month, day, contributors)
+
+    def show(self):
+        raise NotImplementedError
