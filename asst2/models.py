2039b45d57ab5159278d2bd00a0ad63005c0aca8
Christine Jou <chriskjou@gmail.com>
2017-03-20 20:42:15 -0400

    new assignment

--- asst2/models.py
+++ asst2/models.py
@@ -1,4 +1,4 @@
-
+from PIL import Image
 from datetime import date
 
 
@@ -12,17 +12,31 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = os.path.getmtime(path).st_birthtime
 
         # list of contirbutors
         self.contributors = contributors
 
     # this defines a show method that has nothing in it, to be overridden later
     def show(self):
-        raise NotImplementedError
+    	raise NotImplementedError
 
 
 # TODO: Define an Article class that extends the Content class
+class Article(Content):
+	def __init__(self, headline, content):
+		self.headline = headline
+		self.content = content
 
+	def show(self):
+		return '{0}: {1}'.format(self.headline, self.content)
 
 # TODO: Define a Picture class that extends the Content class
+class Picture(Content):
+	def __init__(self, title, caption, path):
+		self.title = title
+		self.caption = caption
+		self.im = Image.open(path)
+
+	def show(self):
+		return '{0}: {1} {2}'.format(self.title, self.caption, self.im.show())
