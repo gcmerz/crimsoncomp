82632267af3c40a5080f90970c75a7a91b5fc784
Christina Bear <christinabear@Christinas-MacBook-Pro.local>
2017-04-04 23:24:26 -0400

    finished

--- newspaper/content/admin.py
+++ newspaper/content/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from .models import Article
-
 from .models import Contributor
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'title', 'subtitle', 'contributors', 'text']
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Contributor)
--- newspaper/content/views.py
+++ newspaper/content/views.py
@@ -4,19 +4,18 @@ from .models import Content, Article
 
 def article(request, title):
 	articles = Article.objects.all()
-	print title
 	chosen = articles[0]
 	for a in range(len(articles)):
 	    if articles[a].title == title:
 	        chosen = articles[a]
 	        break
-	
-	print chosen.title
 	t = chosen.title
-	print "check"
-	return render(request, 'content/article.html', chosen)
+	
+	return render(request, 'content/article.html', {'chosen': chosen})
 
 def all_articles(request):
 	articles = Article.objects.all() 
 	return render(
 		request,'content/all_articles.html', {'articles': articles})
+
+
