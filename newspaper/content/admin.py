c3bd6f3c3aaf44ea4c7d9d28f60664f54bbfbcf4
Christina Bear <christinabear@Christinas-MacBook-Pro.local>
2017-04-04 20:41:27 -0400

    two errors still

--- newspaper/content/admin.py
+++ newspaper/content/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+from .models import Contributor
+
+admin.site.register(Article)
+admin.site.register(Contributor)
