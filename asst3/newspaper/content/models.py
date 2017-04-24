a31fd3c54c315d4c211d6f38cb2b4032f8aae5b7
dansoberanes <dansoberanes@gmail.com>
2017-04-24 03:02:52 -0400

    Finished Assn 4

--- asst3/newspaper/content/models.py
+++ asst3/newspaper/content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Content(models.Model):
@@ -8,7 +9,8 @@ class Content(models.Model):
                                           related_name='content')
     pub_date = models.DateTimeField('date published')
 
-class Article(Content):
+
+class Article(Content, models.Model):
     title = Content.title
     subtitle = Content.subtitle
     contributors = Content.contributors
@@ -16,27 +18,21 @@ class Article(Content):
 
     text = models.TextField('article text', default='TEXT')
 
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ('title',)
-
     def show(self):
         string = "Title: " + self.title + "\nSubtitle: " + self.subtitle \
         + "\nPublished: " + self.pub_date + "\n\n" + self.text
         return string
+
+    def __str__(self):
+        return self.title
 
 
 class Contributor(models.Model):
     first_name = models.CharField(max_length=500)
     last_name = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
-    class Meta:
-        ordering = ('first_name', 'last_name')
-
     def die(self):
         self.models.delete()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
