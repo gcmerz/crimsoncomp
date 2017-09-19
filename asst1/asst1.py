0944438a41128d93297c70072927e45401ba003c
Noah Houghton <noah.houghton.97@gmail.com>
2017-09-19 11:01:17 -0700

    basics complete; need to pythnoic-ize implementations

---
+++ asst1/asst1.py
@@ -0,0 +1,48 @@
+import collections
+
+def swapchars(word):
+  letters = collections.Counter(word)
+  # figure out which letter is most common
+  most, _ = letters.most_common()[0]
+  if most == ' ':
+      most, _ = letters.most_common()[1]
+  # figure out which letter is least common
+  least, _ = letters.most_common()[-1]
+  print ('replacing ' + most + 'with ' + least)
+  return word.replace(most, least)
+
+def sortcat(num, *args):
+    li = []
+    for a in args:
+        li.append(a)
+    li.sort(key = len)
+    li.reverse()
+    st = ''
+    if num == (-1):
+        for l in li:
+            st == st + l
+        return st
+    n = 0
+    while num != 0:
+        st = st + li[n]
+        num = num - 1
+        n = n + 1
+    return st
+
+def bluesclues(ab):
+    abbrev = {}
+    with open('states.txt') as f:
+        lines = [line.rstrip('\n') for line in f]
+        for l in lines:
+            x, y = tuple(l.split(','))
+            abbrev.update({y:x})
+    return abbrev[ab]
+
+def bluesbooze(name):
+    abbrev = {}
+    with open('states.txt') as f:
+        lines = [line.rstrip('\n') for line in f]
+        for l in lines:
+            x, y = tuple(l.split(','))
+            abbrev.update({x:y})
+    return abbrev[name]
