57d8c42844a84c7d712fb6da777715bf769795a6
theChinster <31998124+theChinster@users.noreply.github.com>
2017-10-09 16:22:01 -0400

    Finished the Assignment

--- asst2/models.py
+++ asst2/models.py
@@ -1,5 +1,5 @@
-
-from datetime import date
+import datetime
+from PIL import Image
 
 
 class Content(object):
@@ -12,7 +12,7 @@ class Content(object):
 
         # TODO: Delete the following line and replace it with a line
         # that stores the year, month, and day (hint: check out datetime.date)
-        self.creation_date = None
+        self.creation_date = datetime.date(year,month,day)
 
         # list of contirbutors
         self.contributors = contributors
@@ -22,7 +22,23 @@ class Content(object):
         raise NotImplementedError
 
 
-# TODO: Define an Article class that extends the Content class
+# TODO: Defined an Article
+class Article(Content):
+    def __init__(self, year, month, day, contributors, headline, content):
+        Content.__init__(self, year, month, day, contributors)
+        self.headline = headline
+        self.content = content
+    def show(self):
+        print(self.headline+"\n"+self.content)
 
-
-# TODO: Define a Picture class that extends the Content class
+# TODO: Defined a Picture Class
+class Picture(Content):
+    def __init__(self,year,month,day,contributors,title,caption,path):
+        Content.__init__(self,year,month,day,contributors)
+        self.title = title
+        self.caption = caption
+        self.path = path
+    def show(self):
+        print(self.title+"\n"+self.caption)
+        pic = Image.open(self.path,mode='r')
+	pic.show()
